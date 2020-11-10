17f03a46e971d1a138c55fae1e2a94cfc269c474
reinel215 <reinelencuestas@gmail.com>
2020-11-10 13:22:08 -0400

    estilos join listos

--- client/src/components/join/Join.js
+++ client/src/components/join/Join.js
@@ -1,8 +1,30 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import TextField from '@material-ui/core/TextField';
+import Button from "@material-ui/core/Button";
+
+import { lightBlue } from "@material-ui/core/colors";
+
+import { createMuiTheme, ThemeProvider, withStyles } from "@material-ui/core/styles";
 
 
 import './join.css'
+
+
+
+
+const theme = createMuiTheme({
+
+    palette : {
+        secondary : {
+            main : "#ffffff"
+        }
+    }
+
+});
+
+
+
 
 const Join = () => {
 
@@ -15,16 +37,21 @@ const Join = () => {
         <div className="joinOuterContainer">
 
             <div className="joinInnerContainer">
-                <h1 className="heading"> Join to a room </h1>
+                <h1 className="heading"> JOIN TO A ROOM </h1>
 
-                <div><input placeholder="name" className="" onChange={ (event) =>  setName(event.target.value) } /></div>
-                <div><input placeholder="room" className="" onChange={ (event) =>  setRoom(event.target.value) } /></div>
+                <ThemeProvider theme={theme}>
+
+                    <form className="form-join" noValidate autoComplete="off">
+                        <TextField className="join-input" id="name" label="Name" variant="outlined" onChange={(event) => setName(event.target.value)} />
+                        <TextField className="join-input" id="room" label="Room" variant="outlined" onChange={(event) => setRoom(event.target.value)} />
+                    </form>
+
+                    <Link className="link-join" onClick={event => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
+                        <Button color="secondary" variant="outlined" size="large" >Sign In</Button>
+                    </Link>
 
 
-                <Link onClick={event => (!name || !room) ? event.preventDefault() : null } to={`/chat?name=${name}&room=${room}`}>
-                    <button className="button" type="submit"> Sign In </button>
-                </Link>
-
+                </ThemeProvider>
 
             </div>
 
