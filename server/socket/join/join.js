b7d1eb2edc0f5330ff36419272f832942690fce1
reinel215 <reinelencuestas@gmail.com>
2020-11-13 16:53:05 -0400

    [NEW] listo el primero ejemplo del refactoring de los sockets

---
+++ server/socket/join/join.js
@@ -0,0 +1,41 @@
+const { UserService } = require('../../services/users/userService');
+const userService = new UserService();
+
+
+
+const join = (socket) => {
+
+    socket.on('join', ({ name, room }, callback) => {
+
+
+
+        try {
+            let user = userService.addUser({ id: socket.id, name, room });
+            socket.join(user.room);
+            socket.emit('message', { user: 'admin', text: `${user.name} welcome to the room:${user.room}` });
+            socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has join` });
+
+            socket.broadcast.to(user.room).emit('roomData', { room: user.room, users: userService.getUsersInRoom(user.room) });
+
+
+            callback();
+
+        } catch (err) {
+
+            console.error("hubo un error en 'join' agregando a un usario");
+            console.error(err);
+            callback(err.message);
+        }
+
+        console.log(`the name is: ${name} and the room is: ${room}`);
+
+
+
+
+    });
+
+}
+
+
+
+module.exports = join;
--- server/socket/main.js
+++ server/socket/main.js
@@ -1,12 +1,17 @@
-const sockect = require('socket.io');
+const socket = require('socket.io');
 
 const { UserService } = require('../services/users/userService');
 const userService = new UserService();
 
 
+
+//EVENTO JOIN
+const join = require('./join/join');
+
+
 const socketConfig = (server) => {
 
-    const io = sockect(server, {
+    const io = socket(server, {
 
         cors: {
             // The `*` is used as the wildcard here.
@@ -30,31 +35,8 @@ const socketConfig = (server) => {
 
 
 
-        socket.on('join', ({ name, room }, callback) => {
-
-            try {
-
-                console.log();
-
-                let user = userService.addUser({ id: socket.id, name, room });
-                socket.join(user.room);
-                socket.emit('message', { user: 'admin', text: `${user.name} welcome to the room:${user.room}` });
-                socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has join` });
-
-                io.to(user.room).emit('roomData', { room: user.room, users: userService.getUsersInRoom(user.room) });
-
-
-                callback();
-
-            } catch (err) {
-
-                console.error("hubo un error en 'join' agregando a un usario");
-                console.error(err);
-                callback(err.message);
-            }
-
-            console.log(`the name is: ${name} and the room is: ${room}`);
-        });
+        //EVENTO JOIN
+        join(socket);
 
 
         socket.on('sendMessage', (message, callback) => {
